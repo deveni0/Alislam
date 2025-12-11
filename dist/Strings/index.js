2618f28690f9dbc9bcac05af45e2128ac6e7aaa7
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-12 01:27:45 +0200

    Update index.js

--- dist/Strings/index.js
+++ dist/Strings/index.js
@@ -1,17 +1,21 @@
-const fs = require('fs');
-
-const files = fs.readdirSync(__dirname);
-const exports = {};
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Texts = void 0;
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const exportsObj = {};
+const files = fs_1.default.readdirSync(__dirname);
 files.forEach(file => {
-  if (file.endsWith('.js') && file !== 'index.ts') {
-    const moduleName = file.replace('.js', '');
-    const module = require(`./${moduleName}`);
-    
-    Object.keys(module).forEach(key => {
-      exports[key] = module[key];
-    });
-  }
+    if (file.endsWith('.js') && file !== 'index.js') {
+        const moduleName = path_1.default.basename(file, '.js');
+        const modulePath = `./${moduleName}`;
+        const module = require(modulePath);
+        Object.keys(module).forEach(key => {
+            exportsObj[key] = module[key];
+        });
+    }
 });
-
-exports.Texts = exports;
+exports.Texts = exportsObj;
