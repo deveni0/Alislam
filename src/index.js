c1dddec01ccb96b11cb49e0e05d234c8532d1fdf
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-11 22:04:40 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+export async function txtQuran(surahNumber) {
+  const formattedNumber = surahNumber.toString().padStart(3, '0');
+  const url = `https://raw.githubusercontent.com/deveni0/deen-storage/refs/heads/main/TEXTS/Surahs/${formattedNumber}.json`;
+  
+  const response = await fetch(url);
+  const data = await response.json();
+  
+  return data.text;
+}
