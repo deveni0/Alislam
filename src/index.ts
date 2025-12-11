22429a8911159fdbe561d858881059f609b581e7
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-11 21:57:22 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-export function greet(): string {
-  return "صباح الخير 🌅";
+export interface QuranSurah {
+  number: number;
+  name: string;
+  text: string;
+}
+
+export async function txtQuran(surahNumber: number): Promise<string> {
+  const formattedNumber = surahNumber.toString().padStart(3, '0');
+  const url = `https://raw.githubusercontent.com/deveni0/deen-storage/refs/heads/main/TEXTS/Surahs/${formattedNumber}.json`;
+  
+  const response = await fetch(url);
+  const data: QuranSurah = await response.json();
+  
+  return data.text;
 }
