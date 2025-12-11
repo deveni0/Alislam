b3e334fc130018cc412cdd6891c42bb0a0878772
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-12 01:52:28 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-export * from './Strings/index';
+import fs from 'fs';
+import path from 'path';
+
+const exportsObj: any = {};
+const folders = fs.readdirSync(__dirname).filter(item => {
+  const itemPath = path.join(__dirname, item);
+  return fs.statSync(itemPath).isDirectory();
+});
+
+folders.forEach(folder => {
+  const indexPath = path.join(__dirname, folder, 'index.ts');
+  if (fs.existsSync(indexPath)) {
+    const module = require(`./${folder}/index`);
+    Object.keys(module).forEach(key => {
+      exportsObj[key] = module[key];
+    });
+  }
+});
+
+export default exportsObj;
