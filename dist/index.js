ad2a1ba707689afb4b7acf767e64164ada89f27c
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-12 01:51:41 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,22 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./Strings/index.js"), exports);
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const exportsObj = {};
+const folders = fs_1.default.readdirSync(__dirname).filter(item => {
+    const itemPath = path_1.default.join(__dirname, item);
+    return fs_1.default.statSync(itemPath).isDirectory();
+});
+folders.forEach(folder => {
+    const indexPath = path_1.default.join(__dirname, folder, 'index.js');
+    if (fs_1.default.existsSync(indexPath)) {
+        const module = require(`./${folder}/index.js`);
+        Object.keys(module).forEach(key => {
+            exportsObj[key] = module[key];
+        });
+    }
+});
+exports.default = exportsObj;
