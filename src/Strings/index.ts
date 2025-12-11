087d9ede322f54cc49d1bb4c27b7a684237588b4
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-12 00:52:13 +0200

    Update index.ts

--- src/Strings/index.ts
+++ src/Strings/index.ts
@@ -1,5 +1,17 @@
-import { Surah } from './Surah';
+import fs from 'fs';
 
-export const Texts = {
-  Surah
-};
+const files = fs.readdirSync(__dirname);
+const exports: any = {};
+
+files.forEach(file => {
+  if (file.endsWith('.ts') && file !== 'index.ts') {
+    const moduleName = file.replace('.ts', '');
+    const module = require(`./${moduleName}`);
+    
+    Object.keys(module).forEach(key => {
+      exports[key] = module[key];
+    });
+  }
+});
+
+export const Texts = exports;
