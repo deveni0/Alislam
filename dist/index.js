68823f21ed7739035c0f311d753e49114106a216
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-12 02:03:13 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -5,18 +5,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
-const exportsObj = {};
+const mainExports = {};
 const folders = fs_1.default.readdirSync(__dirname).filter(item => {
     const itemPath = path_1.default.join(__dirname, item);
     return fs_1.default.statSync(itemPath).isDirectory();
 });
 folders.forEach(folder => {
-    const indexPath = path_1.default.join(__dirname, folder, 'index.js');
-    if (fs_1.default.existsSync(indexPath)) {
-        const module = require(`./${folder}/index.js`);
-        Object.keys(module).forEach(key => {
-            exportsObj[key] = module[key];
-        });
-    }
+    const folderExports = {};
+    const folderPath = path_1.default.join(__dirname, folder);
+    const files = fs_1.default.readdirSync(folderPath);
+    files.forEach(file => {
+        if (file.endsWith('.js') && file !== 'index.js') {
+            const moduleName = path_1.default.basename(file, '.js');
+            const modulePath = `./${folder}/${moduleName}`;
+            const module = require(modulePath);
+            Object.keys(module).forEach(key => {
+                folderExports[key] = module[key];
+            });
+        }
+    });
+    mainExports[folder] = folderExports;
 });
-exports.default = exportsObj;
+module.exports = mainExports;
