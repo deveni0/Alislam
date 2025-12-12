96ac6e1ef24ebe163290ea28864c6945fce432a6
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-12 17:30:00 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const mainExports = {};
@@ -29,3 +30,4 @@ folders.forEach(folder => {
 Object.keys(mainExports).forEach(key => {
     exports[key] = mainExports[key];
 });
+exports.default = mainExports;
