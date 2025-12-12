e2301fa78e12622be2b35cfb22233ccba14a0748
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-12 16:51:56 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
+```javascript
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const mainExports = {};
@@ -28,3 +28,4 @@ folders.forEach(folder => {
     }
 });
 module.exports = mainExports;
+```
