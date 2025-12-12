b4de26daac4bb3426656893d1205383bd76f1803
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-12 18:21:26 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,30 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-const fs_1 = __importDefault(require("fs"));
-const path_1 = __importDefault(require("path"));
+
+const fs = require("fs");
+const path = require("path");
 
 const mainExports = {};
 
-const folders = fs_1.default.readdirSync(__dirname).filter(item => {
-    const itemPath = path_1.default.join(__dirname, item);
-    return fs_1.default.statSync(itemPath).isDirectory();
+const folders = fs.readdirSync(__dirname).filter(item => {
+    const itemPath = path.join(__dirname, item);
+    return fs.statSync(itemPath).isDirectory();
 });
 
 folders.forEach(folder => {
     const folderExports = {};
-    const folderPath = path_1.default.join(__dirname, folder);
-    const files = fs_1.default.readdirSync(folderPath);
+    const folderPath = path.join(__dirname, folder);
+    const files = fs.readdirSync(folderPath);
 
     files.forEach(file => {
-        if (file.endsWith('.js') && file !== 'index.js') {
-            const moduleName = path_1.default.basename(file, '.js');
+        if (file.endsWith(".js") && file !== "index.js") {
+            const moduleName = path.basename(file, ".js");
             const modulePath = `./${folder}/${moduleName}`;
-            const module = require(modulePath);
-            folderExports[moduleName] = module.default || module;
+            const mod = require(modulePath);
+
+            folderExports[moduleName] =
+                mod && typeof mod === "object" && "default" in mod
+                    ? mod.default
+                    : mod;
         }
     });
 
@@ -30,6 +32,5 @@ folders.forEach(folder => {
         mainExports[folder] = folderExports;
     }
 });
-
 
 module.exports = mainExports;
