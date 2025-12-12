851f38b099a1f64631bd693d02e35f5a80d33d7c
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-12 20:03:44 +0200

    Update Surah.ts

--- src/Strings/Surah.ts
+++ src/Strings/Surah.ts
@@ -1,15 +1,17 @@
 export interface QuranSurah {
-  number: number;
-  name: string;
-  text: string;
+    number: number;
+    name: string;
+    text: string;
 }
 
-export async function Surah(surahNumber: number): Promise<string> {
-  const formattedNumber = surahNumber.toString().padStart(3, '0');
-  const url = `https://raw.githubusercontent.com/deveni0/deen-storage/refs/heads/main/TEXTS/Surahs/${formattedNumber}.json`;
-  
-  const response = await fetch(url);
-  const data: QuranSurah = await response.json();
-  
-  return data;
+export async function Surah(surahNumber: number): Promise<QuranSurah> {
+    const formattedNumber = surahNumber.toString().padStart(3, '0');
+    const url = `https://raw.githubusercontent.com/deveni0/deen-storage/refs/heads/main/TEXTS/Surahs/${formattedNumber}.json`;
+    
+    const response = await fetch(url);
+    const data: QuranSurah = await response.json();
+    
+    return data;
 }
+
+export default Surah;
