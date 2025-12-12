cdd1618edc91f4256b00d609a83e910a42db8954
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-12 18:36:46 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,30 +1,26 @@
 "use strict";
-
 const fs = require("fs");
 const path = require("path");
 
 const mainExports = {};
 
 const folders = fs.readdirSync(__dirname).filter(item => {
-    const itemPath = path.join(__dirname, item);
-    return fs.statSync(itemPath).isDirectory();
+    const full = path.join(__dirname, item);
+    return fs.statSync(full).isDirectory();
 });
 
 folders.forEach(folder => {
-    const folderExports = {};
     const folderPath = path.join(__dirname, folder);
     const files = fs.readdirSync(folderPath);
+    const folderExports = {};
 
     files.forEach(file => {
         if (file.endsWith(".js") && file !== "index.js") {
             const moduleName = path.basename(file, ".js");
             const modulePath = `./${folder}/${moduleName}`;
             const mod = require(modulePath);
-
-            folderExports[moduleName] =
-                mod && typeof mod === "object" && "default" in mod
-                    ? mod.default
-                    : mod;
+            const value = mod && typeof mod === "object" && "default" in mod ? mod.default : mod;
+            folderExports[moduleName] = value;
         }
     });
 
@@ -32,5 +28,7 @@ folders.forEach(folder => {
         mainExports[folder] = folderExports;
     }
 });
+
+mainExports.__esModule = true;
 
 module.exports = mainExports;
