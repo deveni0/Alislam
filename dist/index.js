6425247211c32d2acbbb92d7f08cac0b14cad37f
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-12 17:12:39 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -26,7 +26,6 @@ folders.forEach(folder => {
         mainExports[folder] = folderExports;
     }
 });
-exports.default = mainExports;
 Object.keys(mainExports).forEach(key => {
     exports[key] = mainExports[key];
 });
