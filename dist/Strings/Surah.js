fd871b24d5e3c83ce18f498a38e576f90720aa1a
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-17 21:21:36 +0200

    Delete dist directory

--- dist/Strings/Surah.js
+++
@@ -1,6 +0,0 @@
-export async function Surah(surahNumber) {
-    const res = await fetch("https://raw.githubusercontent.com/deveni0/deen-storage/refs/heads/main/TEXTS/Surahs.json")
-    const txt = await res.json()
-    return txt[surahNumber - 1]
-}
-export default Surah;
