072c67d7f39e759cce75bba01068ad03b5b2225e
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-17 23:54:00 +0200

    Update AzkAr.ts

--- src/texts/AzkAr.ts
+++ src/texts/AzkAr.ts
@@ -9,7 +9,7 @@
 
 type AzkarType = 1 | 2 | 3 | 4 | 5 | 'morning' | 'evening' | 'sleeping' | 'food' | 'prayer';
 
-export const getAzkar = async (type: AzkarType): Promise<any[]> => {
+export const azkar = async (type: AzkarType): Promise<any[]> => {
   const azkar: any[][] = await (await fetch(
     "https://raw.githubusercontent.com/deveni0/deen-storage/refs/heads/main/TEXTS/Azkar.json"
   )).json();
