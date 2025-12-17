ee7f3e6eae8670b3a49d3514b8e51f08ff6e6e22
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-18 00:50:18 +0200

    Update index.ts

--- src/texts/index.ts
+++ src/texts/index.ts
@@ -1,6 +1,6 @@
 export { AzkAr } from "./AzkAr";
 export { Hadith } from "./Hadith";
-export { Surah } from "./Surah"
+export { Surah } from "./Surah";
 export { Tafseer } from "./Tafseer";
 export { Tasbih } from "./Tasbih";
 export { NamesAllah } from "./NamesAllah";
