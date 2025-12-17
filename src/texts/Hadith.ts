18ea3c2ab9e42449b5e1efbdd783c4c37b49b09a
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-17 23:46:42 +0200

    Create Hadith.ts

---
+++ src/texts/Hadith.ts
@@ -0,0 +1,22 @@
+interface Hadith {
+  id: number;
+  book: string;
+  author: string;
+  deathYear: string;
+  status: string;
+  volume: string;
+  hadithNumber: string;
+  chapter: string;
+  heading: string;
+  arabic: string;
+  english: string;
+  urdu: string;
+}
+
+export const getHadith = async (num: number): Promise<Hadith> => {
+  const hadiths: Hadith[] = await (await fetch(
+    "https://raw.githubusercontent.com/deveni0/deen-storage/refs/heads/main/TEXTS/hadiths.json"
+  )).json();
+  
+  return hadiths[num - 1]; // limited to number -> 7000
+};
