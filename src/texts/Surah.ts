c0db2b2638fb00cf6e4b7e5d632270dfcbd3f52e
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-17 23:27:10 +0200

    Create Surah.ts

---
+++ src/texts/Surah.ts
@@ -0,0 +1,7 @@
+export const getSurah = async (num: number): Promise<any> => {
+  const surahs: any[] = await (await fetch(
+    "https://raw.githubusercontent.com/deveni0/deen-storage/refs/heads/main/TEXTS/Surahs.json"
+  )).json();
+  
+  return surahs[num - 1];
+};
