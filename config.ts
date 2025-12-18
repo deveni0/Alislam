ab5fc9335967f92ae31a53e5ebf7ae97c0e4f8eb
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-18 22:03:05 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-export const API_CONFIG = {
+export const CONFIG = {
   BASE_URL: 'https://raw.githubusercontent.com/deveni0/deen-storage/refs/heads/main',
   QURAN_API: 'https://quran-muslims.vercel.app/api'
 };
