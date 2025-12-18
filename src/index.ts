f9a6c98ffb3d4192d4cdea7f61b926b318aa30ce
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-18 05:29:33 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export * as alaswat from "./alAswat";
-export * as texts from "./texts";
+export * as kataba from "./texts";
+export * as toolkit from "./Toolkit";
+export * as search from "./Search";
+
