37b2b3df26a7c2e4e78b7bd0d9cc9d7bd99c69ec
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2025-12-19 02:22:52 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * as alaswat from "./alAswat";
 export * as kataba from "./texts";
 export * as toolkit from "./Toolkit";
 export * as search from "./Search";
+export * as random  from "./Random";
 
