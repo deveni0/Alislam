5153108fa4e9ec4968d57724a0276f30600ec975
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2026-01-02 07:47:42 +0200

    Update verseGame.ts

--- src/Games/verseGame.ts
+++ src/Games/verseGame.ts
@@ -1,4 +1,4 @@
-export const Questions = async (): Promise<any[] | null> => {
+export const verseGame = async (): Promise<any[] | null> => {
   try {
     const response = await fetch('https://raw.githubusercontent.com/deveni0/deen-storage/refs/heads/main/games/verse_game.json');
     const data: any[] = await response.json();
