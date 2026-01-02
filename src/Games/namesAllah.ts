fd07f2b1c53ba40382e0d5f744da74e58867f3b9
@VENi - Yousefv 🕷️ <teamxovapi@gmail.com>
2026-01-02 07:45:19 +0200

    Create namesAllah.ts

---
+++ src/Games/namesAllah.ts
@@ -0,0 +1,10 @@
+export const namesAllah = async (): Promise<any[] | null> => {
+  try {
+    const response = await fetch('https://raw.githubusercontent.com/deveni0/deen-storage/refs/heads/main/games/names_allah.json');
+    const data: any[] = await response.json();
+    return data;
+
+  } catch {
+    return null;
+  }
+};
